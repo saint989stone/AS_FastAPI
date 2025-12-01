76d60a8959613b4f7b3c6841a136abdcc4e7b5ee
saint989stone <stone_89@bk.ru>
2025-12-01 17:17:40 +0300

    add_test_data

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,27 @@
 import pytest
+from httpx import AsyncClient
 
 from src.config import settings
 from src.database import Base, engine_null_pool
+from src.main import app
 from src.models import *
 
-@pytest.fixture(scope="session",autouse=True)
-async def async_main():
+@pytest.fixture(scope="session", autouse=True)
+def check_test_mode():
     assert settings.MODE == "TEST"
+
+@pytest.fixture(scope="session",autouse=True)
+async def setup_database(check_test_mode):
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+@pytest.fixture(scope="session",autouse=True)
+async def register_user(setup_database):
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        await ac.post(
+            "/auth/register",
+            json={"email": "bob@cat.ru",
+                  "password": "bobcat",
+                  }
+        )
